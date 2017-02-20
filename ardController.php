3f0af864514f777a8680c2a069395fa1ae568f51
felipe8313 <felipe.r.p.1994@gmail.com>
2017-02-20 18:59:11 +0100

    Conecto arduino al nuevo módulo WIFI y hago las peticiones GET

--- ardController.php
+++ ardController.php
@@ -9,49 +9,60 @@
 //error_reporting(E_ALL);
 //ini_set('display_errors', '1');
 include 'clases/bd.class.php';
-    
-//$idDispositivo = $_GET['id'];
-//$usuario = $_GET['usuario'];
-$idDispositivo = 'M1Z4B1A1';
-$usuario = '0619182220';
+
+$idDispositivo = $_GET['id'];
+$usuario = $_GET['usuario'];
+//$idDispositivo = 'M1Z4B1A1';
+//$usuario = '0619182220';
 
 $bd = new bd();
 
-// Obtengo los datos del asiento
-$datos = $bd->consulta("select Estado, Usuario_reserva, Usuario_ocupacion from Asiento where Id = '" . $idDispositivo . "'");
-
-$estado = (int) $datos[0]['Estado'];
-
-if ($estado === 0) { // Asiento ocupado
-    $usuarioOcupacion = $datos[0]['Usuario_ocupacion'];
-
-    // Si el usuario pasa de nuevo la tarjeta libero el sitio
-    if ($usuario === $usuarioOcupacion) {
-
-        $bd->update("update Asiento set Estado = 1, Usuario_ocupacion = NULL, HoraOcupacion = NULL where Id = '" . $idDispositivo . "'");
-
-        echo "1";
-    } else {
-        echo 'El sitio está ocupado';
-    }
-} else if ($estado === 1) { // Asiento libre
-//
-    // Ocupo el asiento
-    $bd->update("update Asiento set Estado = 0, Usuario_ocupacion = '" . $usuario . "', HoraOcupacion = now(), Usuario_reserva = NULL, HoraReserva = NULL where Id = '" . $idDispositivo . "'");
-    echo "0";
-
-} else if ($estado === 2) { // Asiento reservado
+// Si no se especifica un usuario significa que solo quiero obtener el estado actual del sitio
+if ($usuario === "") {
     
-    $usuarioReserva = $datos[0]['Usuario_reserva'];
+    // Obtengo los datos del asiento
+    $datos = $bd->consulta("select Estado from Asiento where Id = '" . $idDispositivo . "'");
 
-    // Si es el usuario que ha reservado el sitio
-    if ($usuario === $usuarioReserva) {
+    $estado = (int) $datos[0]['Estado'];
 
+    echo "Respuesta:".$estado;
+    
+}else{
+
+// Obtengo los datos del asiento
+    $datos = $bd->consulta("select Estado, Usuario_reserva, Usuario_ocupacion from Asiento where Id = '" . $idDispositivo . "'");
+
+    $estado = (int) $datos[0]['Estado'];
+
+    if ($estado === 0) { // Asiento ocupado
+        $usuarioOcupacion = $datos[0]['Usuario_ocupacion'];
+
+        // Si el usuario pasa de nuevo la tarjeta libero el sitio
+        if ($usuario === $usuarioOcupacion) {
+
+            $bd->update("update Asiento set Estado = 1, Usuario_ocupacion = NULL, HoraOcupacion = NULL where Id = '" . $idDispositivo . "'");
+
+            echo "Respuesta:1";
+        } else {
+            echo 'El sitio está ocupado';
+        }
+    } else if ($estado === 1) { // Asiento libre
+//
         // Ocupo el asiento
         $bd->update("update Asiento set Estado = 0, Usuario_ocupacion = '" . $usuario . "', HoraOcupacion = now(), Usuario_reserva = NULL, HoraReserva = NULL where Id = '" . $idDispositivo . "'");
-        echo "0";
-    } else {
-        echo "3";
+        echo "Respuesta:0";
+    } else if ($estado === 2) { // Asiento reservado
+        $usuarioReserva = $datos[0]['Usuario_reserva'];
+
+        // Si es el usuario que ha reservado el sitio
+        if ($usuario === $usuarioReserva) {
+
+            // Ocupo el asiento
+            $bd->update("update Asiento set Estado = 0, Usuario_ocupacion = '" . $usuario . "', HoraOcupacion = now(), Usuario_reserva = NULL, HoraReserva = NULL where Id = '" . $idDispositivo . "'");
+            echo "Respuesta:0";
+        } else {
+            echo "Respuesta:3";
+        }
     }
 }
 
--- barcodeReader/barcodeReader.ino
+++ barcodeReader/barcodeReader.ino
@@ -1,7 +1,14 @@
 #include <hidboot.h>
 #include <usbhub.h>
 #include <Wire.h>
-#include <Ciao.h>
+
+/////////// Librerías para el módulo WiFi ///////////////
+#include "WiFiEsp.h"
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 5); // RX, TX
+#endif
+////////////////////////////////////////////////////////
 
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
@@ -23,88 +30,115 @@ int pinVerde = 3;
 unsigned long previousMillis = 0;
 unsigned long interval = 3000;
 bool lectura = false;
+char ssid[] = "JAZZTEL_bcUJ";            // your network SSID (name)
+char pass[] = "nhpbrpf4nuag";        // your network password
+char server[] = "librarino.ticsur.es";
+String id = "M1Z4B1A1";
 
-
+// Initialize the Ethernet client object
+WiFiEspClient client;
 
 class KbdRptParser : public KeyboardReportParser
 {
-protected:
-	virtual void OnKeyDown	(uint8_t mod, uint8_t key);
-	virtual void OnKeyPressed(uint8_t key);
+  protected:
+    virtual void OnKeyDown	(uint8_t mod, uint8_t key);
+    virtual void OnKeyPressed(uint8_t key);
 };
- 
-void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)	
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
-    uint8_t c = OemToAscii(mod, key);
- 
-    if (c)
-        OnKeyPressed(c);
+  uint8_t c = OemToAscii(mod, key);
+
+  if (c)
+    OnKeyPressed(c);
 }
- 
+
 /* what to do when symbol arrives */
-void KbdRptParser::OnKeyPressed(uint8_t key){
- 
-  barcodeDigit=((char)key);  // Read characters that arrive from serial port
+void KbdRptParser::OnKeyPressed(uint8_t key) {
+
+  barcodeDigit = ((char)key); // Read characters that arrive from serial port
   barcode += barcodeDigit;         //each character builds in a string
   cont++;
- 
-  if (char(key)== 19) {    //verify the las digit of the scanner
-  
-      barcode.remove(cont-1);  //Remove the last digit
-      Serial.println(barcode); //Printing the complete code
-      barcode="";                       // Reset the counter
-      cont = 0;
-      lectura = true;
-                  
+
+  if (char(key) == 19) {   //verify the las digit of the scanner
+
+    barcode.remove(cont - 1); //Remove the last digit
+    Serial.println(barcode); //Printing the complete code
+    cont = 0;
+    lectura = true;
+
   }
-  
+
 };
 
-void color(String estado){
-  
-  switch(estado.toInt()){
-      // rojo - ocupado
-      case 0: analogWrite(pinRojo,255); 
-                analogWrite(pinAzul,0); 
-                analogWrite(pinVerde,0);
+void color(String estado) {
+
+  switch (estado.toInt()) {
+    // rojo - ocupado
+    case 0: analogWrite(pinRojo, 255);
+      analogWrite(pinAzul, 0);
+      analogWrite(pinVerde, 0);
       break;
-      
-      // verde - libre
-      case 1: analogWrite(pinRojo,0); 
-                analogWrite(pinAzul,0); 
-                analogWrite(pinVerde,255);
+
+    // verde - libre
+    case 1: analogWrite(pinRojo, 0);
+      analogWrite(pinAzul, 0);
+      analogWrite(pinVerde, 255);
       break;
-      
-      // naranja - reservado
-      case 2: analogWrite(pinRojo,245); 
-                analogWrite(pinAzul,33); 
-                analogWrite(pinVerde,064);
+
+    // naranja - reservado
+    case 2: analogWrite(pinRojo, 245);
+      analogWrite(pinAzul, 33);
+      analogWrite(pinVerde, 064);
       break;
-    }
+  }
 }
 
 
-void compruebaSitio(String url){
+void compruebaSitio(String barcode) {
 
-    CiaoData data = Ciao.write("rest", "librarino.ticsur.es", url);
-    if (!data.isEmpty()){
-      estadoSitio = data.get(2);
+  if (client.connect(server, 80)) {
+    Serial.println("Connected to server");
+    // Make a HTTP request
+    client.println("GET /librarinoApp/ardController.php?id=" + id + "&usuario=" + barcode + " HTTP/1.1");
+    client.println("Host: librarino.ticsur.es");
+    client.println("Connection: close");
+    client.println();
+  }
+
+  // Leo la respuesta del servidor
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    String subline = line.substring(0, 11);
+    String subline2 = line.substring(11);
+    subline.trim();
+
+    if (subline == "Respuesta:") {
+      Serial.println(subline + " ->> " + subline2);
+      estadoSitio = subline2;
       color(estadoSitio);
-      Serial.println( "Response: " + estadoSitio);
-    }else{ 
-      Serial.println ("Write Error");
-    } 
+    }
+  }
 
+  delay(300);
+  client.stop();
 }
 
 KbdRptParser Prs;
 
 void setup()
 {
-  
-  Serial.begin( 115200 );
+
+  Serial.begin( 9600 );
   Serial.println("Start ");
-  Ciao.begin();
+
+  // initialize serial for ESP module
+  Serial1.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial1);
+
+  // Connect to WPA/WPA2 network
+  WiFi.begin(ssid, pass);
 
   if (Usb.Init() == -1)
     Serial.println("OSC did not start.");
@@ -112,25 +146,26 @@ void setup()
   delay( 200 );
 
   HidKeyboard.SetReportParser(0, &Prs);
-  
+
 }
 
 void loop()
-{  
-  
+{
+
   // Cada 3 segundos compruebo el estado del asiento por si se ha reservado ya
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
-    compruebaSitio("/librarinoApp/ardCompAsiento.php");    
+    compruebaSitio("");
   }
-  
+
   Usb.Task();
-    
-  if (lectura){
-    compruebaSitio("/librarinoApp/ardController.php");
+
+  if (lectura) {
+    compruebaSitio(barcode);
     lectura = false;
-  }  
+    barcode = ""; 
+  }
 }
 
 
